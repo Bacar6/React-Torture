6c383ebc52c8a922a13e2666dd75a4d25cfe2d02
gala <andhoumani13003.g@gmail.com>
2023-06-27 12:07:37 +0200

    fichier création composent navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+//Ce code est écrit en utilisant la bibliothèque React pour créer une application web. L'application affiche une liste de cartes Pokémon et permet de naviguer entre elles à l'aide de deux boutons "Précédent" et "Suivant".
+
+//Commençons par les importations. La première ligne importe React et useState depuis la bibliothèque React. Ensuite, PokemonCard est importé depuis un fichier de composant local ./components/PokemonCard. Ces importations permettent d'utiliser les fonctionnalités de React dans le code.
 import React, { useState } from 'react';
 import PokemonCard from './components/PokemonCard';
 
+
+//Ensuite, une variable pokemonList est définie. Elle contient un tableau d'objets représentant différents Pokémon. Chaque objet contient le nom et l'URL de l'image du Pokémon. Notez que le dernier objet, représentant le Pokémon "mew", n'a pas d'URL d'image.
 const pokemonList = [
   {
     name: "bulbasaur",
@@ -27,32 +32,42 @@ const pokemonList = [
   },
 ];
 
+
+//Ensuite, le composant App est défini. Il utilise le hook useState pour gérer un état local pokemonIndex, qui représente l'index du Pokémon actuellement affiché dans la liste.
 const App = () => {
   const [pokemonIndex, setPokemonIndex] = useState(0);
 
-  const handlePrevious = () => {
+
+ // Deux fonctions buttonPrecedent et buttonSuivant sont définies pour mettre à jour l'index pokemonIndex lorsque les boutons "Précédent" et "Suivant" sont cliqués. Si l'index est dans les limites du tableau pokemonList, il est mis à jour en conséquence.
+  const buttonPrecedent = () => {
     if (pokemonIndex > 0) {
       setPokemonIndex(pokemonIndex - 1);
     }
   };
 
-  const handleNext = () => {
+  const buttonSuivant = () => {
     if (pokemonIndex < pokemonList.length - 1) {
       setPokemonIndex(pokemonIndex + 1);
     }
   };
 
+
+//  Ensuite, la variable currentPokemon est définie en utilisant l'index pokemonIndex pour accéder au Pokémon correspondant dans le tableau pokemonList. 
   const currentPokemon = pokemonList[pokemonIndex];
 
+
+//  Enfin, dans le rendu du composant App, on trouve deux boutons "Précédent" et "Suivant" qui appellent respectivement les fonctions buttonPrecedent et buttonSuivant lorsqu'ils sont cliqués. La carte Pokémon actuelle est affichée en utilisant le composant PokemonCard en lui passant le Pokémon correspondant en tant que prop.
   return (
     <div>
-      <button onClick={handlePrevious}>Précédent</button>
-      <button onClick={handleNext}>Suivant</button>
+      <button onClick={buttonPrecedent}>Précédent</button>
+      <button onClick={buttonSuivant}>Suivant</button>
       <PokemonCard pokemon={currentPokemon} />
     </div>
   );
 };
 
+
+// Le code exporte ensuite le composant App par défaut, ce qui signifie qu'il peut être utilisé ailleurs dans l'application ou dans d'autres fichiers.
 export default App;
 
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,10 @@
+//import React from 'react'; : Cette ligne importe le module React, qui est nécessaire pour définir les composants React.
 import React from 'react';
+
+//import PropTypes from 'prop-types'; : Cette ligne importe le module PropTypes, qui permet de définir les types des props attendues par un composant. Cela permet de valider les données reçues et de détecter d'éventuelles erreurs.
 import PropTypes from 'prop-types';
 
+//const pokemonList = [...] : Cette partie du code définit une variable pokemonList qui contient un tableau d'objets représentant différents Pokémon. Chaque objet a une propriété name pour le nom du Pokémon et imgSrc pour l'URL de l'image correspondante.
 const pokemonList = [
   {
     name: "bulbasaur",
@@ -27,21 +31,33 @@ const pokemonList = [
   },
 ];
 
-
-
+//const PokemonCard = ({ pokemon }) => { : Cette ligne définit le composant PokemonCard en utilisant une fonction fléchée. Il prend une prop pokemon en tant que paramètre destructuré.
 const PokemonCard = ({ pokemon }) => {
   return (
+
+//figure : Cette balise HTML représente une figure qui contient le contenu de la carte Pokémon.
     <figure>
+
+{/* {pokemon.imgSrc != null ? ( ... ) : ( ... )} : Cette ligne utilise une expression ternaire pour effectuer un rendu conditionnel. Si pokemon.imgSrc n'est pas nul (null), alors le premier bloc (...) est exécuté, sinon le second bloc (...) est exécuté. */}
       {pokemon.imgSrc != null ? (
+
+//{/*  <img src={pokemon.imgSrc} alt={pokemon.name} /> : Si pokemon.imgSrc n'est pas nul, cette ligne affiche une balise img avec la source de l'image (src) définie sur pokemon.imgSrc et l'attribut alt défini sur pokemon.name.   */}   
         <img src={pokemon.imgSrc} alt={pokemon.name} />
       ) : (
+
+//{/* <p>???</p> : Si pokemon.imgSrc est nul, cette ligne affiche une balise p avec le texte "???". */}    
         <p>???</p>
       )}
+
+//{/* <figcaption>{pokemon.name}</figcaption> : Cette ligne affiche le nom du Pokémon à l'intérieur d'une balise figcaption.*/} 
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 };
 
+ //{/* PokemonCard.propTypes = { ... } : Cette partie du code définit les prop types attendues par le composant PokemonCard. On utilise PropTypes.shape pour spécifier que pokemon doit être un objet avec une structure spécifique.
+// name: PropTypes.string.isRequired, : Cette ligne indique que la propriété name de l'objet pokemon doit être une chaîne de caractères requise.
+// imgSrc: PropTypes.string, : Cette ligne indique que la propriété imgSrc de l'objet pokemon est une chaîne de caractères optionnelle.*/} 
 PokemonCard.propTypes = {
   pokemon: PropTypes.shape({
     name: PropTypes.string.isRequired,
@@ -49,6 +65,7 @@ PokemonCard.propTypes = {
   }).isRequired,
 };
 
+//export default PokemonCard; : Cette ligne exporte le composant PokemonCard par défaut, ce qui signifie qu'il peut être importé et utilisé dans d'autres parties de l'application.
 export default PokemonCard;
 
 
